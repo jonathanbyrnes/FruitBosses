559c133ac61bae50b977501a2d2cbd76460467fe
BluntPikachu <48105542+BluntPikachu@users.noreply.github.com>
2025-06-12 18:49:41 +0100

    feat: add config and instantiate command manager as well

--- src/main/java/byrnes/jonathan/FruitBosses.java
+++ src/main/java/byrnes/jonathan/FruitBosses.java
@@ -1,12 +1,21 @@
 package byrnes.jonathan;
 
+import byrnes.jonathan.config.ConfigHelper;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.incendo.cloud.annotations.AnnotationParser;
+import org.incendo.cloud.execution.ExecutionCoordinator;
+import org.incendo.cloud.paper.LegacyPaperCommandManager;
 
 public final class FruitBosses extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        LegacyPaperCommandManager<CommandSender> commandManager = LegacyPaperCommandManager.createNative(this, ExecutionCoordinator.simpleCoordinator());
+        AnnotationParser<CommandSender> parser = new AnnotationParser<>(commandManager, CommandSender.class);
+
+        ConfigHelper configHelper = new ConfigHelper(this);
 
     }
 
